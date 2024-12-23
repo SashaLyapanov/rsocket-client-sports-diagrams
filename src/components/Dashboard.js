68c83937784852a8f07dd27b4928b56202154b7e
Поцелуйко Иван Анатольевич <119080618+PeculiarRP@users.noreply.github.com>
2024-12-23 19:17:35 +0300

    some fix

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import {useEffect, useState} from "react";
 import {useParams} from "react-router";
-import {JsonSerializers, RSocketClient} from "rsocket-core";
+import {JsonSerializer, IdentitySerializer, RSocketClient} from "rsocket-core";
 import RSocketWebSocketClient from "rsocket-websocket-client/build";
 
 const Dashboard = () => {
@@ -11,14 +11,19 @@ const Dashboard = () => {
         // Асинхронная функция внутри useEffect
         const connectAndSubscribe = async () => {
             try {
+                console.log("Попытка установить соединение...");
                 const client = new RSocketClient({
-                    serializers: JsonSerializers,
+                    serializers: {
+                        data: JsonSerializer,
+                        metadata: IdentitySerializer
+                    },
                     transport: new RSocketWebSocketClient({
-                        url: 'ws://localhost:7000',
+                        url: 'ws://localhost:7000/rsocket',
+                        // wsCreator: (url) => new WebSocket(url),
                     }),
                     setup: {
-                        dataMimeType: 'application/json',
-                        metadataMimeType: 'application/json',
+                        dataMimeType: 'application/json', //'text/plain',
+                        metadataMimeType: 'message/x.rsocket.routing.v0',
                         keepAlive: 10000,
                         lifetime: 20000,
                     },
@@ -27,11 +32,24 @@ const Dashboard = () => {
                 // Устанавливаем соединение
                 const rsocket = await client.connect();
 
+                if (!rsocket) {
+                    console.log("Bad")
+                }
+
+                // Кодируем метаданные в байты и добавляем длину
+                const metadataString = "fetch-coach-data";
+
+                console.log(JSON.stringify({coachId}))
+
                 // Подписываемся на поток данных
-                const subscription = rsocket.requestStream({
-                    data: { coach_id: coachId },
+                return rsocket.requestStream({
+                    data: JSON.stringify({coachId}),
                     metadata: String.fromCharCode("fetch-coach-data".length) + "fetch-coach-data",
                 }).subscribe({
+                    onSubscribe: sub => {
+                        console.log("Start")
+                        sub.request(1);
+                    },
                     onNext: (payload) => {
                         console.log("Получены данные:", payload.data);
                         setData((prevData) => [...prevData, payload.data]);
@@ -39,10 +57,8 @@ const Dashboard = () => {
                     onError: (error) => console.error("Ошибка:", error),
                     onComplete: () => console.log("Поток завершен"),
                 });
-
-                return subscription; // Возвращаем подписку для последующей отмены
             } catch (error) {
-                console.error("Ошибка подключения:", error);
+                console.error("Ошибка при подключении:", error);
             }
         };
 
